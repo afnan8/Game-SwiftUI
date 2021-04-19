ddba92767b8b36dc1557eed4e539c26296e9d008
afnan8 <afnan.hassan1993@gmail.com>
2021-04-20 01:28:49 +0200

    Initial Commit

---
+++ Week11/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Week11
+//
+//  Created by Afnan MacBook Pro on 20/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Week11/Week11App.swift
@@ -0,0 +1,17 @@
+//
+//  Week11App.swift
+//  Week11
+//
+//  Created by Afnan MacBook Pro on 20/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct Week11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
